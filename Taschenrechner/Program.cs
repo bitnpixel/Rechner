d82e318a10185bd3f9fe82cf6c6e3f65d8d06f80
bitnpixel <derbaga@gmail.com>
2020-05-02 19:26:00 +0200

    Projektdateien hinzufügen.

---
+++ Taschenrechner/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Begrüßung des Nutzers
+            Console.WriteLine("Herlich Willkommen zum Konsolen-Taschenrechner");
+            Console.WriteLine();
+
+            //Abfrage der beiden Zahlen
+            Console.WriteLine("Bitte gib die erste Zahl ein");
+            string eingabeErsteZahl = Console.ReadLine("");
+            Console.WriteLine();
+            Console.WriteLine("Bitte gib die zweite Zahl ein");
+            string eingabeZweiteZahl = Console.ReadLine("");
+
+            //Text in Gleitkommazahl umwandeln
+            double ersteZahl = Convert.ToDouble(eingabeErsteZahl);
+            double zweiteZahl = Convert.ToDouble(eingabeZweiteZahl);
+
+            //Berechnung Ergebnis
+            double summe = ersteZahl + zweiteZahl;
+        }
+    }
+}
