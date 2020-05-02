576df0a59e97b1dedcd2a6bfd915e4a5677409e9
bitnpixel <derbaga@gmail.com>
2020-05-02 19:34:36 +0200

    Benutzereingabe und Addition als Methode hinzugefügt

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -16,17 +16,34 @@ namespace Taschenrechner
 
             //Abfrage der beiden Zahlen
             Console.WriteLine("Bitte gib die erste Zahl ein");
-            string eingabeErsteZahl = Console.ReadLine("");
+            string eingabeErsteZahl = Console.ReadLine();
             Console.WriteLine();
             Console.WriteLine("Bitte gib die zweite Zahl ein");
-            string eingabeZweiteZahl = Console.ReadLine("");
+            string eingabeZweiteZahl = Console.ReadLine();
 
             //Text in Gleitkommazahl umwandeln
             double ersteZahl = Convert.ToDouble(eingabeErsteZahl);
             double zweiteZahl = Convert.ToDouble(eingabeZweiteZahl);
 
             //Berechnung Ergebnis
-            double summe = ersteZahl + zweiteZahl;
+            double summe = Addition(ersteZahl,zweiteZahl);
+
+            //Ausgabe des Ergebnisses
+            Console.WriteLine("Das Ergebnis lautet: {0}", summe);
+            WartenAufBenutzerEingabe();
+        }
+
+        static void WartenAufBenutzerEingabe()
+        {
+            Console.WriteLine("Bitte drücke die Enter-Taste zum beenden...");
+            Console.ReadLine();
+        }
+
+        static double Addition (double summand1, double summand2)
+        {
+            double ergebnis = summand1 + summand2;
+
+            return ergebnis;
         }
     }
 }
