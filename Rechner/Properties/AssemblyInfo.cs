b4d59112775a7cb3f6cdf1cff4005ed285c380ab
bitnpixel <derbaga@gmail.com>
2020-05-03 13:28:34 +0200

    Projektmappe in Rechner umbenannt auf Grund von Push Problemen

--- Rechner/Properties/AssemblyInfo.cs
+++ Rechner/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("Taschenrechner")]
+[assembly: AssemblyTitle("Rechner")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Taschenrechner")]
+[assembly: AssemblyProduct("Rechner")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
